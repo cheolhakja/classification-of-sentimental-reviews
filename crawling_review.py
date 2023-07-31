4c0f5c0286875d17ca8c9a1d91f66d8a89eb3dbc
cheolhakja <rebegin15@gmail.com>
2023-07-31 16:02:52 +0900

    chore: 크롤링 코드에 주석 달기

--- crawling_review.py
+++ crawling_review.py
@@ -10,29 +10,25 @@ browser.get("https://place.map.kakao.com/1622750377")
 time.sleep(2) #비동기로 처리해서 sleep이 꼭 있어야 하나? 이게 쓰레드 일시중지인가
 
 element = browser.find_element(By.XPATH, "//div[@class='evaluation_review']/a[@class='link_more']")
-
-print(element)
-
 element.send_keys(Keys.ENTER)
-
 time.sleep(2)
+'''
+xpath로 '더보기' 버튼을 element로 가져옴
+클릭함
+'''
 
-#div class="comment_info"
-#p class="txt_comment"
 elements_comment = browser.find_elements(By.XPATH, "//p[@class='txt_comment']/span")
-
-print(len(elements_comment))
-
-for comment in elements_comment:
-    print(comment.text, "\n")
-
 time.sleep(2)
+'''
+xpath로 댓글들을 element list로 가져옴
+클릭함
+'''
 
 star_web_elemets =browser.find_elements(By.XPATH, "//span[@class = 'ico_star star_rate']/span");
-
 stars = []
-
 for star in star_web_elemets:
     stars.append(star.get_attribute("style"))
-
-print(stars)
+'''
+xpath로 별점들을 element list로 가져옴
+(아직 100, 80, 40 이런 식으로 표기됨)
+'''
