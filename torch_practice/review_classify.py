2ffc6b7a2487dbb99424c50bf8d1083a00eccf59
cheolhakja <rebegin15@gmail.com>
2023-08-10 18:16:32 +0900

    test: 모듈 테스트

--- torch_practice/review_classify.py
+++ torch_practice/review_classify.py
@@ -3,3 +3,8 @@ import review_preprocess
 star_ratings = review_preprocess.load_star_ratings()
 print(star_ratings)
 print(len(star_ratings))
+
+reviews = review_preprocess.load_reviews()
+for review in reviews:
+    print(review)
+print(len(reviews))
